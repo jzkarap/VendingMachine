e64c567fa9496111c4f2f0ceb8dd93aab80180d2
John Karaplis <jzkarap@gmail.com>
2018-06-14 14:24:26 -0400

    first commit

---
+++ VendingMachine/VendingMachine/Items/Item.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine.Items
+{
+    public class Item
+    {
+
+		public Item(string name, decimal cost, string type)
+		{
+			this.Name = name;
+			this.Cost = cost;
+			this.Type = type;
+			//this.Count = 5;
+		}
+
+		//public int Count { get; }
+
+		public string Name { get; }
+
+		public string Type { get; }
+
+		public decimal Cost { get; }
+
+		public string Noise()
+		{
+			string noise = "";
+
+			if (this.Type == "Chip")
+			{
+				noise = "Crunch, Crunch, Yum!";
+			}
+
+			if (this.Type == "Candy")
+			{
+				noise = "Munch, Munch, Yum!";
+			}
+
+			if (this.Type == "Gum")
+			{
+				noise = "Chew, Chew, Yum!";
+			}
+
+			if (this.Type == "Drink")
+			{
+				noise = "Glug, Glug, Yum!";
+			}
+
+			return noise;
+		}
+    }
+}
---
+++ VendingMachine/VendingMachine/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using VendingMachine.Items;
+using VendingMachine.IO;
+
+namespace VendingMachine
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			GetItems();
+
+			Stack<Item> purchases = new Stack<Item>();
+
+			GetItems();
+
+			foreach (var kvp in GetItems())
+			{
+				Console.WriteLine(kvp.Key + " " + kvp.Value.Pop().Name);
+			}
+		
+	
+			Dictionary<string, Stack<Item>> GetItems()
+			{
+				int i;
+				int lineCount = 0;
+
+				Dictionary<string, Stack<Item>> slots = new Dictionary<string, Stack<Item>>();
+
+				using (StreamReader sr = new StreamReader("inventory.txt"))
+				{
+					while (!sr.EndOfStream)
+					{
+						Stack<Item> stockPerSlot = new Stack<Item>();
+						// Reads each line from inventory.txt
+						string line = sr.ReadLine();
+						lineCount++;
+						// Splits lines at pipe symbol
+						string[] itemDetails = line.Split('|');
+
+						// Generates item
+						Item itemToVend = new Item(itemDetails[1], decimal.Parse(itemDetails[2]), itemDetails[3]);
+
+						// Push itemToVend into stack stockPerSlot 5 times
+						for (i = 0; i < 5; i++)
+						{
+							stockPerSlot.Push(itemToVend);
+						}
+
+						// If slots (which represents a position in vending array)
+						// does not exist,
+						// add it
+						if (!slots.ContainsKey(itemDetails[0]))
+						{
+							// Attach current stock for slot to position in vending array
+							slots.Add(itemDetails[0], stockPerSlot);
+						}
+
+					}
+
+				}
+				return slots;
+			}
+		}
+	}
+}
